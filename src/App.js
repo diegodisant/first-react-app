d3e283e80000a8efa8fbf222f4bb9d5f472f8f79
diegodisant <developerdiego0@gmail.com>
2026-05-05 12:42:53 -0600

    my first react app 2026 :rocket:

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [advice, setAdvice] = useState('')
+  const [advice, setAdvice] = useState('');
+  const [readCount, setReadCount] = useState(0);
 
   async function fetchAdvice () {
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
     const { advice } = data.slip;
 
+    setReadCount((count) => count + 1);
+
     setAdvice(advice);
   }
 
-  return <div>
-    <h1>First React App 2026 | Random Advice</h1>
-    <h2>Advice: {advice}</h2>
-    <button onClick={fetchAdvice}>Get advice</button>
-  </div>
+  useEffect(function () {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>First React App 2026 | Random Advice</h1>
+      <h2>Advice: {advice}</h2>
+      <Message readCount={readCount}/>
+      <button onClick={fetchAdvice}>Get advice</button>
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+        You have read <strong>{props.readCount}</strong> advices
+    </p>
+  );
 }
